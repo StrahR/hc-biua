6eb0feaf4dc1f40643ff0a322b428bfe01ebd60d
Strah <rs8613@student.uni-lj.si>
2019-02-28 19:18:43 +0100

    remove

--- matching.py
+++ matching.py
@@ -1,7 +1,7 @@
-def similar(a: set, b: set) -> int:
+def similarity(a: set, b: set) -> int:
     s0 = len(a.difference(b))
     s1 = len(b.difference(a))
     s2 = len(a.intersection(b))
     return min(s0, s1, s2)
 
-def find_match
+def find_match(image, images: list, used_images: list):
