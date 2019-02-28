12b97d39cfc84b0c632b52393db6d0ecb15874a9
Gašper <ggasper@ggasper.GasperG>
2019-02-28 19:37:02 +0100

    neki

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
-from hcodeio import in_file
-from matching import find_match, similarity
-
 photos = in_file("a_example.txt")
 
 used_photos = [False] * len(photos)
-used_counter = len(photos)
 
 used_photos[0] = True
-used_counter += 1
-
 
 current_photo = 0
 
 slideshow = [current_photo]
 
-while used_counter < len(photos):
+for i in range(len(photos)):
     new_photo = find_match(current_photo, photos, used_photos)
     used_photos[new_photo] = True
     slideshow.append(new_photo)
