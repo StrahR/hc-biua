6dceaa901a7384518e554dee6db44d592391ff7d
Petrič <tp8663@student.uni-lj.si>
2019-02-28 19:09:33 +0100

    hi

--- parser.py
+++ parser.py
@@ -1,10 +1,13 @@
 
-filename = "a_example.txt"
-images = []
-with open(filename, 'r') as f:
-    line_count = f.readline()
-    for i in range(int(line_count)):
-        orientation, tags_count, *tags = f.readline().split()
-        images.append([orientation, tags])
+
+def in_file(filename)
+    #filename = "a_example.txt"
+    images = []
+    with open(filename, 'r') as f:
+        line_count = f.readline()
+        for i in range(int(line_count)):
+            orientation, tags_count, *tags = f.readline().split()
+            images.append([orientation, tags])
+    return images
 
 print(images)
